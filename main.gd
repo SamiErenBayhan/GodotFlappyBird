d141b2e8e98407424da0a24d6e80fd6f88f6c770
SamiErenBayhan <155374516+SamiErenBayhan@users.noreply.github.com>
2026-06-23 20:52:57 +0300

    Initial commit

---
+++ main.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+var bird_position = Vector2(100, 300)#anlık piksel konum
+var velocity = Vector2.ZERO# anlık hareketi sıfır
+
+const GRAVITY = 600#yerçekimi değeri
+const JUMP_FORCE = -300#yerçekimine karşı gelen eksi zıplama değeri
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta):
+	velocity.y += GRAVITY * delta #Her karede yerçekimini kuşun Y hızına ekliyoruz. Kuş gitgide hızlanarak düşüyor
+	bird_position += velocity * delta #burada da kuşun pozisyonunu düşürüyoruz ve pürüzsük bir şekilde hızlanarak düşüyor
+	queue_redraw()
+
+func _draw():
+	var bird_rect = Rect2(bird_position, Vector2(32,32)) 
+	draw_rect(bird_rect, Color.BLUE)
+	var bird	
+		
