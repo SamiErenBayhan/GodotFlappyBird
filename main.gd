103c96991a53926474f451320147e5a85c1ccb3e
SamiErenBayhan <155374516+SamiErenBayhan@users.noreply.github.com>
2026-06-24 14:16:08 +0300

    last update

--- main.gd
+++ main.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var bird_position = Vector2(100, 300)#anlık piksel konum
 var velocity = Vector2.ZERO# anlık hareketi sıfır
+var is_game_started = false#oyun başlamadı
 
 const GRAVITY = 600#yerçekimi değeri  #Hız
 const JUMP_FORCE = -300#yerçekimine karşı gelen eksi zıplama değeri
@@ -12,17 +13,19 @@ const PIPE_WIDTH = 75
 const PIPE_HEIGHT = 500
 
 const GAP_SIZE = 200 # İki boru arasındaki kuşun geçeceği piksel boşluğu
-
 var score = 0
 
 func _ready() -> void:
 	pass 
 
 func _physics_process(delta):
-	velocity.y += GRAVITY * delta 
-	bird_position += velocity * delta 
+	velocity.y += GRAVITY * delta # yerçekimi ekliyoruz
+	bird_position += velocity * delta # kuşu yerçekiminden etkilendiriyoruz
 	
-	if pipes.size() > 0 and pipes[0] != null:
+	if not is_game_started:#oyun başlamadı hala false ise diğer kodları okuma
+		return
+	
+	if pipes.size() > 0 and pipes[0] != null:#boşa düşen boruları temizleme
 		if pipes[0].x < -100: 
 			pipes.remove_at(0)
 	
@@ -30,30 +33,40 @@ func _physics_process(delta):
 		if i < pipes.size(): 
 			pipes[i].x -= PIPE_SPEED * delta 
 	
-	for i in range(pipes.size()):
+	for i in range(pipes.size()):#skor mekaniği
 		if i < pipes.size():
 			if pipes[i].x < 100 and pipes[i].x > 100 - (PIPE_SPEED * delta):
 				score += 1
+				$ScoreSound.play()
+				$ScoreLabel.text = str(score)
 	
-	var bird_rect = Rect2(bird_position, Vector2(32, 32))
-	for i in range(pipes.size()):
-		var rects = get_pipe_rects(i)
+	var bird_rect = Rect2(bird_position, Vector2(32, 32))#kuşun büyüklüğünün tanımı
+	
+	for i in range(pipes.size()):#borulara çarpma
+		var rects = get_pipe_rects(i)#fonksiyonu bu koda çekiyoruz
 		if rects[0] != Rect2():
 			if bird_rect.intersects(rects[0]) or bird_rect.intersects(rects[1]):
 				game_over()
 				return
 	
+	if bird_position.y > 568:#aşağı düşme
+		game_over()
+		return
+	
+	if bird_position.y < 0:#yukarı çıkamama
+		bird_position.y = 0 
+		velocity.y = 0
 	queue_redraw()
 
-func _draw():
-	# 1. Kuşumuzu çiziyoruz
-	var bird_rect = Rect2(bird_position, Vector2(32, 32)) 
-	draw_rect(bird_rect, Color.BLUE)
+func _draw():  
 	
-	if pipes.size() == 0:
+	draw_rect(Rect2(0, 0, 1000, 700), Color(0.3, 0.6, 0.9))#arka plan
+	var bird_rect = Rect2(bird_position, Vector2(32, 32)) 
+	draw_rect(bird_rect, Color.YELLOW)#kuşun boyutu ve çizimi
+	
+	if pipes.size() == 0:#oyun çöktüğü için yaptığımız bazı şeyler ileride bunlarla uğraşmıcaz
 		return
 		
-	# 2. Boruları çiziyoruz (Yazı falan yok, tertemiz!)
 	for i in range(pipes.size()):
 		if i >= pipes.size(): break
 		var pipe_pos = pipes[i]
@@ -64,12 +77,18 @@ func _draw():
 		var top_pos_y = pipe_pos.y - GAP_SIZE - PIPE_HEIGHT
 		var top_rect = Rect2(Vector2(pipe_pos.x, top_pos_y), Vector2(PIPE_WIDTH, PIPE_HEIGHT))
 		draw_rect(top_rect, Color.GREEN)
+
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
-			if $Pipe_timer.is_stopped():
-				$Pipe_timer.start()
+			if not is_game_started:
+				is_game_started = true#boşluğa basılınca bunu true ya çevir ve her şey başlasaın
+				if $Pipe_timer.is_stopped():
+					$Pipe_timer.start()
+				if has_node("StartLabel"):
+					$StartLabel.visible = false
 			velocity.y = JUMP_FORCE
-		
+			$JumpSound.play()
+
 func _on_pipe_timer_timeout() -> void:
 	var random_y = randi_range(200, 500)
 	var new_pipe = Vector2(800, random_y)
@@ -80,13 +99,14 @@ func game_over():
 	velocity = Vector2.ZERO
 	pipes.clear()
 	score = 0 
-	if has_node("PipeTimer"):
-		$PipeTimer.stop()
-	elif has_node("Timer"): # Eğer adı düz Timer ise onu durdur
-		$Timer.stop()
+	$ScoreLabel.text = str(score)
+	$GameOverSound.play()
+	is_game_started = false#tekrardan durdur
+	if has_node("StartLabel"):
+		$StartLabel.visible = true
 	queue_redraw()
 	
-func get_pipe_rects(index: int) -> Array:
+func get_pipe_rects(index: int) -> Array:#boruları oluşturma fonksiyonu 
 	if pipes.size() == 0 or index >= pipes.size():
 		return [Rect2(), Rect2()]
 	
